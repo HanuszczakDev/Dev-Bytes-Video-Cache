a4201b67ba86fa520db1839d9bf9fb9b44ce5194
ahanuszczak <hanyhusajn@gmail.com>
2023-03-18 10:44:32 +0100

    create Worker

--- app/src/main/java/com/hanuszczak/devbytesvideocache/work/RefreshDataWork.kt
+++ app/src/main/java/com/hanuszczak/devbytesvideocache/work/RefreshDataWork.kt
@@ -16,3 +16,23 @@
  */
 
 package com.hanuszczak.devbytesvideocache.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.hanuszczak.devbytesvideocache.database.VideosDatabase
+import com.hanuszczak.devbytesvideocache.repository.VideosRepository
+import retrofit2.HttpException
+
+class RefreshDataWork(appContext: Context, params: WorkerParameters): CoroutineWorker(appContext, params) {
+    override suspend fun doWork(): Result {
+        val database = VideosDatabase.getInstance(applicationContext)
+        val repository = VideosRepository(database)
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+    }
+}
